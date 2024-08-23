7dba3cedcfc6766d84d8fb099e53472ff2f8f5ef
lana-z <lana@levelupeconomy.com>
2024-08-22 23:54:52 -0700

    add initial sections to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="flex gap-2 p-8 pointer-events-none place-items-center lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </div>
-      </div>
-
-      <div >
+    <main className="flex flex-col items-center justify-between min-h-screen">
+      {/* Logo Section */}
+      <div className="flex justify-center w-full py-8 bg-white">
         <Image
           src="/levelup-logo-transparent.png"
           alt="LevelUp Logo"
@@ -25,83 +14,81 @@ export default function Home() {
         />
       </div>
 
-      <h1 className="mb-8 text-4xl font-bold text-center">
-        Building vibrant economic ecosystems.
-      </h1>
-      
-      <h3 className="mb-8 text-2xl text-center">  
-      Our clients are diverse organizations, from foundations and enterprises to start-ups, all seeking a partner capable of guiding them from research and strategy to transformative design and world-class products.
-      </h3>
+      {/* Title Section */}
+      <section className="w-full pb-20 text-center bg-white">
+        <h1 className="py-20 text-4xl font-bold">
+          Building vibrant economic ecosystems.
+        </h1>
 
-      <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* Who We Work With Section */}        
+      <section className="w-full py-16 text-center bg-blue-100">
+      <h3 className="py-20 mt-4 text-2xl ">
+          Our clients are diverse organizations, from foundations and enterprises to start-ups, all seeking a partner capable of guiding them from research and strategy to transformative design and world-class products.
+        </h3>
+        <div className="grid mx-auto text-center lg:max-w-5xl lg:w-full lg:grid-cols-3 lg:text-left">
+          
+          <a
+            href="/accelerators"
+            className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-blue-200"
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              Accelerators{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-75">
+              Empower your founder cohorts with proven training and customized workshops that meet them where they are.
+            </p>
+          </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <a
+            href="/founders"
+            className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-blue-200"
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              Founders{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-75">
+              Transform your startup vision into reality with our research, coaching, and custom consulting services.
+            </p>
+          </a>
+          
+          <a
+            href="/funders"
+            className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-blue-200"
+          >
+            <h2 className="mb-3 text-2xl font-semibold">
+              Funders{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className="m-0 max-w-[30ch] text-sm opacity-75">
+              Discover what's missing with our ecosystem analysis and provide holistic support that nurtures high-potential startups and drives strong results.
+            </p>
+          </a>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* Understands Section */}
+      <section className="w-full py-16 text-center bg-green-100">
+        <h2 className="text-2xl">
+          We are your partner who deeply understands the startup ecosystem, founder life, and product development lifecycle. Let's work together to ensure your success.
+        </h2>
+      </section>
+
+      {/* Global Tech Sustainable Section */}
+      <section className="w-full py-16 text-center bg-yellow-100">
+        <h2 className="text-2xl">
+          LevelUp is a global company leveraging technology to advance sustainable economic growth.
+        </h2>
+      </section>
     </main>
   );
 }
